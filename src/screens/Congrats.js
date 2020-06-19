e87e8eafb8fa54158445e2c31c39e0d585682f8a
Gareth Field <field.gareth@gmail.com>
2020-06-19 17:37:28 -0400

    save

--- src/screens/Congrats.js
+++ src/screens/Congrats.js
@@ -1,31 +1,31 @@
-import React from "react";
-import { Header } from "../components";
+import React from 'react';
+import { Header } from '../components';
 
 export const Congrats = ({
-  playerName,
-  level,
-  lastGuessCount,
-  stars,
-  nextLevel,
-  tryAgain,
+	playerName,
+	level,
+	lastGuessCount,
+	stars,
+	nextLevel,
+	tryAgain,
 }) => {
-  return (
-    <Header>
-      <h1>Congratulations {playerName}!</h1>
-      <h2>You won!</h2>
-      {level >= 2 ? (
-        <>
-          <p>That took {lastGuessCount} guesses!</p>
-          <p>
-            You got {stars} stars! {`starUnicode`.repeat(stars)}
-            {`emptyStarUnicode`.repeat(3 - stars)}
-          </p>
-          <button onClick={() => tryAgain()}>Try again?</button>
-        </>
-      ) : null}
-      <button disabled={level > 1 ? true : false} onClick={() => nextLevel()}>
-        Play level {level + 1}!
-      </button>
-    </Header>
-  );
+	return (
+		<Header>
+			<h1>Congratulations {playerName}!</h1>
+			<h2>You won!</h2>
+			{level >= 2 ? (
+				<>
+					<p>That took {lastGuessCount} guesses!</p>
+					<p>
+						You got {stars} stars! {`★`.repeat(stars)}
+						{`☆`.repeat(3 - stars)}
+					</p>
+					<button onClick={() => tryAgain()}>Try again?</button>
+				</>
+			) : null}
+			<button disabled={level > 1 ? true : false} onClick={() => nextLevel()}>
+				Play level {level + 1}!
+			</button>
+		</Header>
+	);
 };
